ab791451c66132faded0217555a21095b15e6ed5
Daniel Errante <danoph@gmail.com>
2013-09-12 03:39:17 -0500

    making tests pass for ip address lookups

--- problem_2/country_ip.rb
+++ problem_2/country_ip.rb
@@ -1,11 +1,44 @@
+require 'ipaddr'
+require 'csv'
+
 class CountryIp
-  
-  def initialize
-    # Implement me
-  end
-  
   def search(ip_string)
-    # Implement me
+    raise ArgumentError unless valid_ip_address?(ip_string)
+
+    ip_int = ip_integer(ip_string)
+
+    File.open(filename).readlines.each do |line|
+      next if line_is_comment?(line)
+
+      row = CSV.parse(line).first
+
+      from_ip = row[0].to_i
+      to_ip = row[1].to_i
+
+      if ip_int >= from_ip && ip_int <= to_ip
+        return row[6]
+      end
+    end
   end
-  
+
+  def valid_ip_address?(ip_string)
+    IPAddr.new(ip_string).ipv4?
+  rescue IPAddr::InvalidAddressError
+    raise ArgumentError
+  end
+
+  def line_is_comment?(line)
+    line.match /^(#|\s)+/
+  rescue ArgumentError
+    true
+  end
+
+  def filename
+    File.expand_path('../IpToCountry.csv', __FILE__)
+  end
+
+  def ip_integer(ip_string)
+    ip = ip_string.split(".")
+    ip[3].to_i + (ip[2].to_i * 256) + (ip[1].to_i * 256 * 256) + (ip[0].to_i * 256 * 256 * 256)
+  end
 end
